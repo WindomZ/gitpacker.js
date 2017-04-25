b9af438232f9dc46b0f4e05fd00aa3f1ce0033fa
WindomZ <git.windomz@gmail.com>
2017-04-25 18:01:50 +0800

    init project

--- bin/gitpacker.js
+++ bin/gitpacker.js
@@ -1,3 +1,43 @@
+#!/usr/bin/env node
 /**
  * Created by WindomZ on 17-4-21.
  */
+'use strict'
+
+const process = require('process')
+
+const program = require('commander')
+
+const pkg = require('../package.json')
+
+let noArgs = true
+
+program
+  .version(pkg.version)
+  .description('Compress files based on git ignore.')
+  .option('-i, --include <file...>', 'include files', null, null)
+  .option('-e, --exclude <file...>', 'exclude files', null, null)
+
+program
+  .command('zip <file>')
+  .action((file, options) => {
+    noArgs = false
+    console.log('zip >>> file: %s', file)
+    console.log('zip >>> include: %s', options.parent.include)
+    console.log('zip >>> exclude: %s', options.parent.exclude)
+  })
+
+program
+  .command('tar <file>')
+  .action((file, options) => {
+    noArgs = false
+    console.log('tar >>> file: %s', file)
+    console.log('tar >>> include: %s', options.parent.include)
+    console.log('tar >>> exclude: %s', options.parent.exclude)
+  })
+
+program.parse(process.argv)
+
+if (noArgs) {
+  program.outputHelp()
+}
