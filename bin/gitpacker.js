5b68c187e8de7a6f2ab054d97a92a9e5b68727a3
WindomZ <git.windomz@gmail.com>
2017-04-26 18:03:53 +0800

    step: base project

--- bin/gitpacker.js
+++ bin/gitpacker.js
@@ -10,30 +10,47 @@ const program = require('commander')
 
 const pkg = require('../package.json')
 
+const compress = require('../lib/gitpacker')
+const {toArray} = require('../lib/utils')
+
 let noArgs = true
 
 program
   .version(pkg.version)
   .description('Compress files based on git ignore.')
-  .option('-i, --include <file...>', 'include files', null, null)
-  .option('-e, --exclude <file...>', 'exclude files', null, null)
+  .option('-i, --include <files ...>', 'include files', null, null)
+  .option('-e, --exclude <files ...>', 'exclude files', null, null)
 
 program
-  .command('zip <file>')
-  .action((file, options) => {
+  .command('zip <file> [dirs...]')
+  .action((file, dirs, options) => {
     noArgs = false
+
     console.log('zip >>> file: %s', file)
+    console.log('zip >>> dirs: %s', dirs)
     console.log('zip >>> include: %s', options.parent.include)
     console.log('zip >>> exclude: %s', options.parent.exclude)
+
+    compress('zip', file, toArray(dirs),
+      toArray(options.parent.include), toArray(options.parent.exclude))
+      .then(item => console.log('then: ', item))
+      .catch(e => console.error('catch: ', e))
   })
 
 program
-  .command('tar <file>')
-  .action((file, options) => {
+  .command('tar <file> [dirs...]')
+  .action((file, dirs, options) => {
     noArgs = false
+
     console.log('tar >>> file: %s', file)
+    console.log('tar >>> dirs: %s', dirs)
     console.log('tar >>> include: %s', options.parent.include)
     console.log('tar >>> exclude: %s', options.parent.exclude)
+
+    compress('tar', file, toArray(dirs),
+      toArray(options.parent.include), toArray(options.parent.exclude))
+      .then(item => console.log('then: ', item))
+      .catch(e => console.error('catch: ', e))
   })
 
 program.parse(process.argv)
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./bin/gitpacker.js zip test ./ -i asset -e lib
