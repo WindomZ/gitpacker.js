050116d14ed91a4158a5b851535900525a6442bb
WindomZ <git.windomz@gmail.com>
2017-04-28 08:32:30 +0800

    feat: add gitpack impl support git ignore

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 rm -f test.zip
 ./bin/gitpacker.js zip test.zip -i asset -e lib,bin
 
-#rm -f test.zip.tar
-#./bin/gitpacker.js tar test.zip -i asset -e lib,bin
+rm -f test.zip.tar
+./bin/gitpacker.js tar test.zip -i asset -e lib,bin
