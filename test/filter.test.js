980db7090955636ecd7e0707e8a47a0806aff3b7
WindomZ <git.windomz@gmail.com>
2017-04-28 13:58:33 +0800

    test: add filter testings

---
+++ test/filter.test.js
@@ -0,0 +1,47 @@
+/**
+ * Created by WindomZ on 17-4-28.
+ */
+const test = require('ava')
+
+const {include, addIncludes, exclude, addExcludes} = require('../lib/filter')
+
+test('filter pass', t => {
+  addIncludes()
+  addExcludes()
+
+  t.false(include('aaa'))
+  t.false(exclude('aaa'))
+
+  addIncludes([''])
+  addExcludes([''])
+
+  t.false(include('aaa'))
+  t.false(exclude('aaa'))
+
+  addIncludes(['aaa'])
+  addIncludes(['bbb'])
+
+  addExcludes(['ccc'])
+  addExcludes(['ddd'])
+
+  t.true(include('aaa'))
+  t.false(exclude('aaa'))
+
+  t.true(include('bbb'))
+  t.false(exclude('bbb'))
+
+  t.false(include('ccc'))
+  t.true(exclude('ccc'))
+
+  t.false(include('ddd'))
+  t.true(exclude('ddd'))
+
+  addIncludes(['*-x'])
+  addExcludes(['*-y'])
+
+  t.true(include('aaa-x'))
+  t.false(exclude('aaa-x'))
+
+  t.false(include('aaa-y'))
+  t.true(exclude('aaa-y'))
+})
